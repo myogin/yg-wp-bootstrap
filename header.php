56f85d637de039d1fbe569c59e80bd5279b3e56a
myogin <yoginugraha19@gmail.com>
2024-04-12 22:39:53 +0800

    Create front-page

--- header.php
+++ header.php
@@ -17,48 +17,59 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="profile" href="https://gmpg.org/xfn/11">
-  <!-- Favicons -->
-  <link rel="apple-touch-icon" sizes="180x180" href="<?= get_stylesheet_directory_uri(); ?>/img/favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="<?= get_stylesheet_directory_uri(); ?>/img/favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="<?= get_stylesheet_directory_uri(); ?>/img/favicon/favicon-16x16.png">
-  <link rel="manifest" href="<?= get_stylesheet_directory_uri(); ?>/img/favicon/site.webmanifest">
-  <link rel="mask-icon" href="<?= get_stylesheet_directory_uri(); ?>/img/favicon/safari-pinned-tab.svg" color="#0d6efd">
-  <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="theme-color" content="#ffffff">
-  <?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180"
+        href="<?= get_stylesheet_directory_uri(); ?>/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32"
+        href="<?= get_stylesheet_directory_uri(); ?>/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16"
+        href="<?= get_stylesheet_directory_uri(); ?>/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?= get_stylesheet_directory_uri(); ?>/img/favicon/site.webmanifest">
+    <link rel="mask-icon" href="<?= get_stylesheet_directory_uri(); ?>/img/favicon/safari-pinned-tab.svg"
+        color="#0d6efd">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
-<?php wp_body_open(); ?>
+    <?php wp_body_open(); ?>
 
-<div id="page" class="site">
+    <div id="page" class="site">
 
-  <header id="masthead" class="site-header">
+        <header id="masthead" class="site-header">
 
-    <div class="fixed-top bg-body-tertiary">
+            <div class="fixed-top bg-body-tertiary">
 
-      <nav id="nav-main" class="navbar navbar-expand-lg">
+                <nav id="nav-main" class="navbar navbar-expand-lg">
 
-        <div class="<?= bootscore_container_class(); ?>">
+                    <div class="<?= bootscore_container_class(); ?>">
 
-          <!-- Navbar Brand -->
-          <a class="navbar-brand xs d-md-none" href="<?= esc_url(home_url()); ?>"><img src="<?= esc_url(get_stylesheet_directory_uri()); ?>/img/logo/logo-sm.svg" alt="logo" class="logo xs"></a>
-          <a class="navbar-brand md d-none d-md-block" href="<?= esc_url(home_url()); ?>"><img src="<?= esc_url(get_stylesheet_directory_uri()); ?>/img/logo/logo.svg" alt="logo" class="logo md"></a>
+                        <!-- Navbar Brand -->
+                        <a class="navbar-brand xs d-md-none" href="<?= esc_url(home_url()); ?>"><img
+                                src="<?= esc_url(get_stylesheet_directory_uri()); ?>/img/logo/logo-sm.svg" alt="logo"
+                                class="logo xs"></a>
+                        <a class="navbar-brand md d-none d-md-block" href="<?= esc_url(home_url()); ?>"><img
+                                src="<?= esc_url(get_stylesheet_directory_uri()); ?>/img/logo/logo.svg" alt="logo"
+                                class="logo md"></a>
 
-          <!-- Offcanvas Navbar -->
-          <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas-navbar">
-            <div class="offcanvas-header">
-              <span class="h5 offcanvas-title"><?php esc_html_e('Menu', 'bootscore'); ?></span>
-              <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
+                        <!-- Offcanvas Navbar -->
+                        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas-navbar">
+                            <div class="offcanvas-header">
+                                <span class="h5 offcanvas-title"><img
+                                        src="<?= esc_url(get_stylesheet_directory_uri()); ?>/img/logo/logo-sm.svg"
+                                        alt="logo" class="logo xs"></span>
+                                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="offcanvas-body">
 
-              <!-- Bootstrap 5 Nav Walker Main Menu -->
-              <?php
+                                <!-- Bootstrap 5 Nav Walker Main Menu -->
+                                <?php
               wp_nav_menu(array(
                 'theme_location' => 'main-menu',
                 'container'      => false,
@@ -70,22 +81,22 @@
               ));
               ?>
 
-              <!-- Top Nav 2 Widget -->
-              <?php if (is_active_sidebar('top-nav-2')) : ?>
-                <?php dynamic_sidebar('top-nav-2'); ?>
-              <?php endif; ?>
+                                <!-- Top Nav 2 Widget -->
+                                <?php if (is_active_sidebar('top-nav-2')) : ?>
+                                <?php dynamic_sidebar('top-nav-2'); ?>
+                                <?php endif; ?>
 
-            </div>
-          </div>
+                            </div>
+                        </div>
 
-          <div class="header-actions d-flex align-items-center">
+                        <div class="header-actions d-flex align-items-center">
 
-            <!-- Top Nav Widget -->
-            <?php if (is_active_sidebar('top-nav')) : ?>
-              <?php dynamic_sidebar('top-nav'); ?>
-            <?php endif; ?>
+                            <!-- Top Nav Widget -->
+                            <?php if (is_active_sidebar('top-nav')) : ?>
+                            <?php dynamic_sidebar('top-nav'); ?>
+                            <?php endif; ?>
 
-            <?php
+                            <?php
             if (class_exists('WooCommerce')) :
               get_template_part('template-parts/header/actions', 'woocommerce');
             else :
@@ -93,18 +104,20 @@
             endif;
             ?>
 
-            <!-- Navbar Toggler -->
-            <button class="btn btn-outline-secondary d-lg-none ms-1 ms-md-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas-navbar" aria-controls="offcanvas-navbar">
-              <i class="fa-solid fa-bars"></i><span class="visually-hidden-focusable">Menu</span>
-            </button>
+                            <!-- Navbar Toggler -->
+                            <button class="btn btn-outline-secondary d-lg-none ms-1 ms-md-2" type="button"
+                                data-bs-toggle="offcanvas" data-bs-target="#offcanvas-navbar"
+                                aria-controls="offcanvas-navbar">
+                                <i class="fa-solid fa-bars"></i><span class="visually-hidden-focusable">Menu</span>
+                            </button>
 
-          </div><!-- .header-actions -->
+                        </div><!-- .header-actions -->
 
-        </div><!-- bootscore_container_class(); -->
+                    </div><!-- bootscore_container_class(); -->
 
-      </nav><!-- .navbar -->
+                </nav><!-- .navbar -->
 
-      <?php
+                <?php
       if (class_exists('WooCommerce')) :
         get_template_part('template-parts/header/top-nav-search-collapse', 'woocommerce');
       else :
@@ -112,12 +125,12 @@
       endif;
       ?>
 
-    </div><!-- .fixed-top .bg-light -->
-    <!-- Offcanvas User and Cart -->
-    <?php
+            </div><!-- .fixed-top .bg-light -->
+            <!-- Offcanvas User and Cart -->
+            <?php
     if (class_exists('WooCommerce')) :
       get_template_part('template-parts/header/offcanvas', 'woocommerce');
     endif;
     ?>
 
-  </header><!-- #masthead -->
+        </header><!-- #masthead -->
