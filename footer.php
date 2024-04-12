231fe18d366effc119ef4c89698624bee71857b2
myogin <yoginugraha19@gmail.com>
2024-04-12 22:39:19 +0800

    Update footer

--- footer.php
+++ footer.php
@@ -16,48 +16,48 @@
 
 <footer>
 
-  <div class="bootscore-footer bg-body-tertiary pt-5 pb-3">
-    <div class="<?= bootscore_container_class(); ?>">
+    <div class="bootscore-footer bg-body-tertiary pt-5 pb-3">
+        <div class="<?= bootscore_container_class(); ?>">
 
-      <!-- Top Footer Widget -->
-      <?php if (is_active_sidebar('top-footer')) : ?>
-        <?php dynamic_sidebar('top footer'); ?>
-      <?php endif; ?>
+            <!-- Top Footer Widget -->
+            <?php if (is_active_sidebar('top-footer')) : ?>
+            <?php dynamic_sidebar('top footer'); ?>
+            <?php endif; ?>
 
-      <div class="row">
+            <div class="row">
 
-        <!-- Footer 1 Widget -->
-        <div class="col-md-6 col-lg-3">
-          <?php if (is_active_sidebar('footer-1')) : ?>
-            <?php dynamic_sidebar('footer-1'); ?>
-          <?php endif; ?>
-        </div>
+                <!-- Footer 1 Widget -->
+                <div class="col-md-6 col-lg-3">
+                    <?php if (is_active_sidebar('footer-1')) : ?>
+                    <?php dynamic_sidebar('footer-1'); ?>
+                    <?php endif; ?>
+                </div>
 
-        <!-- Footer 2 Widget -->
-        <div class="col-md-6 col-lg-3">
-          <?php if (is_active_sidebar('footer-2')) : ?>
-            <?php dynamic_sidebar('footer-2'); ?>
-          <?php endif; ?>
-        </div>
+                <!-- Footer 2 Widget -->
+                <div class="col-md-6 col-lg-3">
+                    <?php if (is_active_sidebar('footer-2')) : ?>
+                    <?php dynamic_sidebar('footer-2'); ?>
+                    <?php endif; ?>
+                </div>
 
-        <!-- Footer 3 Widget -->
-        <div class="col-md-6 col-lg-3">
-          <?php if (is_active_sidebar('footer-3')) : ?>
-            <?php dynamic_sidebar('footer-3'); ?>
-          <?php endif; ?>
-        </div>
+                <!-- Footer 3 Widget -->
+                <div class="col-md-6 col-lg-3">
+                    <?php if (is_active_sidebar('footer-3')) : ?>
+                    <?php dynamic_sidebar('footer-3'); ?>
+                    <?php endif; ?>
+                </div>
 
-        <!-- Footer 4 Widget -->
-        <div class="col-md-6 col-lg-3">
-          <?php if (is_active_sidebar('footer-4')) : ?>
-            <?php dynamic_sidebar('footer-4'); ?>
-          <?php endif; ?>
-        </div>
+                <!-- Footer 4 Widget -->
+                <div class="col-md-6 col-lg-3">
+                    <?php if (is_active_sidebar('footer-4')) : ?>
+                    <?php dynamic_sidebar('footer-4'); ?>
+                    <?php endif; ?>
+                </div>
 
-      </div>
+            </div>
 
-      <!-- Bootstrap 5 Nav Walker Footer Menu -->
-      <?php
+            <!-- Bootstrap 5 Nav Walker Footer Menu -->
+            <?php
       wp_nav_menu(array(
         'theme_location' => 'footer-menu',
         'container'      => false,
@@ -69,22 +69,27 @@
       ));
       ?>
 
+        </div>
     </div>
-  </div>
 
-  <div class="bootscore-info bg-body-tertiary text-body-tertiary border-top py-2 text-center">
-    <div class="<?= bootscore_container_class(); ?>">
-      <?php if (is_active_sidebar('footer-info')) : ?>
-        <?php dynamic_sidebar('footer-info'); ?>
-      <?php endif; ?>
-      <small class="bootscore-copyright"><span class="cr-symbol">&copy;</span>&nbsp;<?= date('Y'); ?> <?php bloginfo('name'); ?></small>
+    <div class="bootscore-info bg-body-tertiary text-body-tertiary py-2 copyright">
+        <div class="<?= bootscore_container_class(); ?>">
+            <?php if (is_active_sidebar('footer-info')) : ?>
+            <?php dynamic_sidebar('footer-info'); ?>
+            <?php endif; ?>
+            <span class="bootscore-copyright  "><span class="cr-symbol">&copy;</span>&nbsp;<?= date('Y'); ?>
+                <span class="text-primary"><a href="#">YG Creative Studio</a></span>
+                | Designed by
+                <span class="text-primary"><a href="#">Yogi Nugraha</a></span></span>
+
+        </div>
     </div>
-  </div>
 
 </footer>
 
 <!-- To top button -->
-<a href="#" class="btn btn-primary shadow top-button position-fixed zi-1020"><i class="fa-solid fa-chevron-up"></i><span class="visually-hidden-focusable">To top</span></a>
+<a href="#" class="btn btn-primary shadow top-button position-fixed zi-1020"><i class="fa-solid fa-chevron-up"></i><span
+        class="visually-hidden-focusable">To top</span></a>
 
 </div><!-- #page -->
 
